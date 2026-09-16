3538d228bfab599b578926377dfeb0ccc1dd0aae
unknown <afzanoor344@gmail.com>
2026-09-16 12:09:36 +0500

    Remove favicon branding

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -106,7 +106,6 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/afza-mark.svg", type: "image/svg+xml" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
